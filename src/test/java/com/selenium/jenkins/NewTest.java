a8651f3b86601123ada600550143326f7a0c66ba
unknown <mrlnrose6@gmail.com>
2024-01-04 10:31:36 +0530

    jenkin test

---
+++ src/test/java/com/selenium/jenkins/NewTest.java
@@ -0,0 +1,46 @@
+package com.selenium.jenkins;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class NewTest {
+	
+
+WebDriver driver;
+
+	@BeforeTest
+	public void precondition1()
+	{
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		
+		driver.get("https://demo.automationtesting.in/Register.html"); // to open URL
+		
+	}
+
+	@org.testng.annotations.Test
+	public void test()
+	{
+		System.out.println("new test");
+		org.openqa.selenium.WebElement addr =		driver.findElement(By.tagName("textarea"));
+		
+		addr.click();
+		addr.clear();
+		addr.sendKeys("Perungudi");
+			
+	}
+
+	@AfterTest
+	public void closebrowser()
+	{
+		driver.close();
+	}
+
+
+}
